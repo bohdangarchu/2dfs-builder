a1370c214e59aa9d935f8f67bab22b26c43ac7fa
Bohdan Garchu <bohdan.garchu@gmail.com>
2026-03-26 17:05:48 +0100

    add --home-dir

--- cmd/root.go
+++ cmd/root.go
@@ -16,49 +16,41 @@ var (
 		Long:  `Requires a 2dfs.yaml file in the current directory or a path to a 2dfs.yaml file. Read docs at https://github.com/2DFS/2dfs-builder`,
 	}
 	homeDir, _     = os.UserHomeDir()
-	basePath       = func() string {
-		if envHome := os.Getenv("TDFS_HOME"); envHome != "" {
-			return envHome
-		}
-		return path.Join(homeDir, ".2dfs")
-	}()
-	BlobStorePath  = path.Join(basePath, "blobs")
-	IndexStorePath = path.Join(basePath, "index")
-	KeysStorePath  = path.Join(basePath, "uncompressed-keys")
+	homeDirFlag    string
+	BlobStorePath  string
+	IndexStorePath string
+	KeysStorePath  string
 )
 
 func Execute() error {
 	rootCmd.CompletionOptions.DisableDefaultCmd = true
+	rootCmd.PersistentFlags().StringVar(&homeDirFlag, "home-dir", path.Join(homeDir, ".2dfs"), "home directory for tdfs data")
+	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
+		basePath := homeDirFlag
+		BlobStorePath = path.Join(basePath, "blobs")
+		IndexStorePath = path.Join(basePath, "index")
+		KeysStorePath = path.Join(basePath, "uncompressed-keys")
+
+		if _, err := os.Stat(basePath); os.IsNotExist(err) {
+			fmt.Print("Creating base path: " + basePath + "\n")
+			if err := os.Mkdir(basePath, 0755); err != nil {
+				return fmt.Errorf("error creating base path: %w", err)
+			}
+		}
+		if _, err := os.Stat(BlobStorePath); os.IsNotExist(err) {
+			os.Mkdir(BlobStorePath, 0755)
+		}
+		if _, err := os.Stat(KeysStorePath); os.IsNotExist(err) {
+			os.Mkdir(KeysStorePath, 0755)
+		}
+		if _, err := os.Stat(IndexStorePath); os.IsNotExist(err) {
+			os.Mkdir(IndexStorePath, 0755)
+		}
+		return nil
+	}
 	return rootCmd.Execute()
 }
 
 func init() {
-
-	// Create a new logger with the custom format
 	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
-
-	// check if basePath exists if not create it
-
-	if _, err := os.Stat(basePath); os.IsNotExist(err) {
-		fmt.Print("Creating base path: " + basePath + "\n")
-		err := os.Mkdir(basePath, 0755)
-		if err != nil {
-			log.Fatalf("Error creating base path: %v", err)
-		}
-	}
-
-	//check if basePath/blobstore exists if not create it
-	if _, err := os.Stat(BlobStorePath); os.IsNotExist(err) {
-		os.Mkdir(BlobStorePath, 0755)
-	}
-
-	//check if basePath/index exists if not create it
-	if _, err := os.Stat(KeysStorePath); os.IsNotExist(err) {
-		os.Mkdir(KeysStorePath, 0755)
-	}
-
-	//check if basePath/index exists if not create it
-	if _, err := os.Stat(IndexStorePath); os.IsNotExist(err) {
-		os.Mkdir(IndexStorePath, 0755)
-	}
 }
